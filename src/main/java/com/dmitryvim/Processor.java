384ffcf8ce842558388335d22270899a985ac3b5
Dmitry Mikhaylovich <dmitry.mikhailovich@gmail.com>
2021-09-20 21:17:46 +0100

    add multithreading

--- src/main/java/com/dmitryvim/Processor.java
+++ src/main/java/com/dmitryvim/Processor.java
@@ -3,15 +3,20 @@ package com.dmitryvim;
 import java.net.URI;
 import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Processor {
 
 
+    private AtomicInteger pagesLoading = new AtomicInteger(0);
+
     private Set<URI> parsed = new HashSet<>();
     private Map<URI, Set<URI>> edges = new HashMap<>();
 
-    // TODO check fuxed capacity
-    private ArrayBlockingQueue<URI> queue = new ArrayBlockingQueue<>(100);
+    // TODO check fixed capacity
+    private ArrayBlockingQueue<URI> queue = new ArrayBlockingQueue<>(1000);
 
     // TODO add ctor params
     private final PageLoader pageLoader = new PageLoader();
@@ -19,27 +24,44 @@ public class Processor {
     private final Verificator verificator = new Verificator();
 
     public Map<URI, Set<URI>> crawl(URI uri) {
-        parsed.add(uri);
-        queue.add(uri);
-        while(!queue.isEmpty() && parsed.size() < 100) {
-            step();
+        addFirst(uri);
+
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        while(!queue.isEmpty() || pagesLoading.get() > 0) {
+            if (!queue.isEmpty()) service.submit(this::step);
         }
-        return edges;
+        service.shutdown();
+
+        return getEdges();
     }
 
     private void step() {
+        if (queue.isEmpty()) return;
+
+        pagesLoading.incrementAndGet();
         var uri = queue.poll();
         var content = pageLoader.load(uri);
         var children = pageReader.parse(uri, content);
         add(uri, children);
+        pagesLoading.decrementAndGet();
     }
 
-    private void add(URI parent, Set<URI> children) {
+    private synchronized void addFirst(URI uri) {
+        parsed.add(uri);
+        queue.add(uri);
+
+    }
+
+    private synchronized void add(URI parent, Set<URI> children) {
         edges.put(parent, children);
         children.forEach(ch -> {
             if (verificator.verify(ch) && parsed.add(ch)) {
                 queue.add(ch);
             }
         });
+    }
+
+    private synchronized Map<URI, Set<URI>> getEdges() {
+        return edges;
     }
 }
